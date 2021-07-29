e7016a7f5092a4a99cf41d218f70e5d3c56cee48
euritrends <info@euritrends.eu>
2021-07-29 19:33:28 +0200

    change txt directory inside js

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ var nodes=[],links=[],data={},networkGraph,configFile=null,configFileExp=null,da
 
   function dataViz(){
     var rowDataConfig;
-          d3.tsv("../txt/config_basicMode.txt",function(dataConfig){
-            d3.tsv("../txt/graph_icons.txt",function(dataIcons){
+          d3.tsv("txt/config_basicMode.txt",function(dataConfig){
+            d3.tsv("txt/graph_icons.txt",function(dataIcons){
               filesIcons=dataIcons;
               rowDataConfig=fillDropDown(dataConfig)
               configFile=dataConfig
